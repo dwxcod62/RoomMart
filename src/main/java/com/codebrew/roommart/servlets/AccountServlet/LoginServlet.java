727e53d0861859f5b006db44171aa3359f5723e5
dwxcod62 <ducna1462@gmail.com>
2024-02-08 20:51:05 +0700

    update

--- src/main/java/com/codebrew/roommart/servlets/AccountServlet/LoginServlet.java
+++ src/main/java/com/codebrew/roommart/servlets/AccountServlet/LoginServlet.java
@@ -1,67 +1,57 @@
 package com.codebrew.roommart.servlets.AccountServlet;
 
-import com.codebrew.roommart.dao.AccountDao;
 import com.codebrew.roommart.dao.SystemDao;
 import com.codebrew.roommart.dto.Account;
 import com.codebrew.roommart.dto.Status;
 import com.codebrew.roommart.utils.EncodeUtils;
 import com.codebrew.roommart.utils.RandomUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+    private static final Logger logger = LoggerFactory.getLogger(LoginServlet.class);
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Account acc = null;
-        SystemDao dao = new SystemDao();
-        String uname = request.getParameter("email");
-        String pwd = EncodeUtils.hashSHA256(request.getParameter("pass"));
-
-        String save = null;
-        String url = "login";
+        String email = request.getParameter("email");
+        String password = EncodeUtils.hashSHA256(request.getParameter("pass"));
+        String remember = request.getParameter("remember");
+        String redirectUrl = "login";
 
         try {
-            acc = dao.getAccountByUsernameAndPassword(uname, pwd);
-            if (acc != null && acc.getStatus() == 1){
-                url = "success";
-                HttpSession session = request.getSession(true);
-                if (session != null){
-                    session.setAttribute("USER", acc);
-                    if ( save != null){
-                        String token = RandomUtils.randomToken(30, uname);
-                        int twoDay = 60 * 60 * 24 * 2;
+            Account account = new SystemDao().getAccountByUsernameAndPassword(email, password);
+            if (account != null) {
+                if (account.getStatus() == 1) {
+                    redirectUrl = "success";
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("USER", account);
+                    if (remember != null) {
+                        String token = RandomUtils.randomToken(30, email);
+                        int twoDaysInSeconds = 60 * 60 * 24 * 2;
                         Cookie cookie = new Cookie("selector", token);
-                        cookie.setMaxAge(twoDay);
+                        cookie.setMaxAge(twoDaysInSeconds);
                         response.addCookie(cookie);
-//                        dao.updateTokenByUserName(token, uname);
                     }
+                    logger.info("{} login success!", email);
+                    session.setAttribute("CURRENT_PAGES", "dashboard");
+                } else if (account.getStatus() == -1) {
+                    request.setAttribute("RESPONSE_MSG", Status.builder().status(false).content("Your account has been locked!").build());
+                } else if (account.getStatus() == 0) {
+                    request.setAttribute("RESPONSE_MSG", Status.builder().status(false).content("Please verify your account in the email!").build());
                 }
-                System.out.println(uname + " login success!");
-                session.setAttribute("CURRENT_PAGES", "dashboard");
-            } else if ( acc != null && acc.getStatus() == -1){
-                request.setAttribute("RESPONSE_MSG", Status.builder().status(false).content("Your account has been locked!").build());
-            } else if ( acc != null && acc.getStatus() == 0) {
-                request.setAttribute("RESPONSE_MSG", Status.builder().status(false).content("Please verify your account in the email!").build());
             } else {
                 request.setAttribute("RESPONSE_MSG", Status.builder().status(false).content("Incorrect username or password!").build());
             }
-        } catch ( Exception e){
-            System.out.println("Error at LoginServlet:  " + e);
+        } catch (Exception e) {
+            logger.error("Error at LoginServlet: {}", e.getMessage());
         } finally {
-            if (acc != null && acc.getStatus() == 1) {
-                response.sendRedirect(url);
-            } else {
-                request.getRequestDispatcher(url).forward(request, response);
-            }
+            response.sendRedirect(redirectUrl);
         }
     }
 }
