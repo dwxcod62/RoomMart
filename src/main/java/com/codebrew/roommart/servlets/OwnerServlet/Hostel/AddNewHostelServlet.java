88e2a6959d4ed27ce7f2f8aa29df0466ae8504a0
lequangtrieu <trieulqde160447@gmail.com>
2024-02-14 12:49:24 +0700

    update list-hostel-page-full-v4 add-new-hostel-finished-v1 add-new-service-in-progress

--- src/main/java/com/codebrew/roommart/servlets/OwnerServlet/Hostel/AddNewHostelServlet.java
+++ src/main/java/com/codebrew/roommart/servlets/OwnerServlet/Hostel/AddNewHostelServlet.java
@@ -26,91 +26,24 @@ public class AddNewHostelServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-        List<HostelService> hostelServiceList = new ArrayList<>();
         Account acc;
-        Map<String, Services> servicesList = new ServiceDAO().getAll();
-
         try {
             req.setCharacterEncoding("UTF-8");
             HttpSession session = req.getSession();
             acc = (Account) session.getAttribute("USER");
-            HostelDAO dao = new HostelDAO();
 
-//            int accountId = acc.getAccId(); //for id = 1
-            int accountId = 21;
+            int accountId = acc.getAccId();
             String hostelName = req.getParameter("hostel-name");
             String hostelAddress = req.getParameter("hostel-address");
             String hostelProvince = req.getParameter("hostel-province");
             String hostelDistrict = req.getParameter("hostel-district");
             String hostelWard = req.getParameter("hostel-ward");
 
-            //electric
-            int electricityPrice = Integer.parseInt(req.getParameter("hostel-electric"));
-            hostelServiceList.add(HostelService.builder()
-                    .serviceID(servicesList.get("Điện").getServiceID())
-                    .servicePrice(electricityPrice).build());
-
-            //water
-            int waterPrice = Integer.parseInt(req.getParameter("hostel-water"));
-            hostelServiceList.add(HostelService.builder()
-                    .serviceID(servicesList.get("Nước").getServiceID())
-                    .servicePrice(waterPrice).build());
-
-            //wifi
-            int internetPrice = Integer.parseInt(req.getParameter("hostel-wifi"));
-            if (internetPrice > 0) {
-                hostelServiceList.add(HostelService.builder()
-                        .serviceID(servicesList.get("Wifi").getServiceID())
-                        .servicePrice(internetPrice).build());
-            }
-
-            //Management
-            int managementPrice = Integer.parseInt(req.getParameter("hostel-manage"));
-            if (managementPrice > 0) {
-                hostelServiceList.add(HostelService.builder()
-                        .serviceID(servicesList.get("Phí quản lý").getServiceID())
-                        .servicePrice(managementPrice).build());
-            }
-
-            //Vehicle
-            int vehiclePrice = Integer.parseInt(req.getParameter("hostel-vehicle"));
-            if (managementPrice > 0) {
-                hostelServiceList.add(HostelService.builder()
-                        .serviceID(servicesList.get("Phí giữ xe").getServiceID())
-                        .servicePrice(vehiclePrice).build());
-            }
-
-            //cleaning
-            int cleaningPrice = Integer.parseInt(req.getParameter("hostel-cleaning"));
-            if (managementPrice > 0) {
-                hostelServiceList.add(HostelService.builder()
-                        .serviceID(servicesList.get("Phí vệ sinh").getServiceID())
-                        .servicePrice(cleaningPrice).build());
-            }
-
-            Hostel hostel = Hostel.builder()
-                    .hostelOwnerAccountID(accountId)
-                    .hostelName(hostelName)
-                    .address(hostelAddress)
-                    .ward(hostelWard)
-                    .district(hostelDistrict)
-                    .city(hostelProvince).build();
-            int hostelId = dao.addHostel(hostel, hostelServiceList);
-
-            if (hostelId >= 0) {
-                req.setAttribute("HOSTEL_ID", hostelId);
-                req.setAttribute("RESPONSE_MSG", HandlerStatus.builder()
-                        .status(true)
-                        .content("Tạo khu trọ thành công!").build());
-            } else {
-                req.setAttribute("RESPONSE_MSG", HandlerStatus.builder()
-                        .status(false)
-                        .content("Đã có lỗi xảy ra! Tạo khu trọ thất bại!").build());
-            }
+            new HostelDAO().addNewHostel(new Hostel(accountId, hostelName, hostelAddress, hostelWard, hostelDistrict, hostelProvince));
         } catch (Exception e) {
             log("Error at AddHostel: " + e.toString());
         } finally {
-            req.getRequestDispatcher("/add-new-hostel").forward(req, response);
+            response.sendRedirect("/RoomMart/owner-hostel-list");
         }
     }
 }
