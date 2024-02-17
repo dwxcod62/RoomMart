0e05c636d251db44ed4483e8ee319b7c8f56a52e
dwxcod62 <ducna1462@gmail.com>
2024-02-17 15:57:22 +0700

    them create contract

--- src/main/java/com/codebrew/roommart/servlets/ContractServlet/CreateContractServlet.java
+++ src/main/java/com/codebrew/roommart/servlets/ContractServlet/CreateContractServlet.java
@@ -34,7 +34,7 @@ public class CreateContractServlet extends HttpServlet {
             request.setAttribute("RESPONSE_MSG", status);
         }
 
-        System.out.println("d");
+        System.out.println("e");
 
         try {
             if (data != null) {
