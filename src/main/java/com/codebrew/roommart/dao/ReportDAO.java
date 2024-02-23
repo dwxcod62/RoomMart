8d9872544651c583473756ae960c063ba5a68e85
IamFinn7 <cpn2003.py@gmail.com>
2024-02-23 10:26:45 +0700

    Update Send Report Renter

--- src/main/java/com/codebrew/roommart/dao/ReportDAO.java
+++ src/main/java/com/codebrew/roommart/dao/ReportDAO.java
@@ -3,15 +3,15 @@ package com.codebrew.roommart.dao;
 import com.codebrew.roommart.dto.Report;
 import com.codebrew.roommart.utils.DatabaseConnector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReportDAO {
     private static final String GET_REPORTS_BY_ID = "SELECT * FROM Reports where send_account_id = ?";
+    private static final String INSERT_REPORT =
+            "INSERT INTO public.reports(send_date, content, status, reply_account_id, send_account_id, cate_id) VALUES(?, ?, ?, ?, ?, ?)";
+
     public List<Report> getReportByRenterId(int id) throws SQLException {
         List<Report> reports = new ArrayList<>();
         Connection cn = null;
@@ -61,5 +61,55 @@ public class ReportDAO {
             }
         }
         return reports;
+    }
+
+    public int addReport(Report report) throws SQLException {
+        boolean check = false;
+        Connection cn = null;
+        int reportId = -1;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+        try {
+            cn = DatabaseConnector.makeConnection();
+            //Insert table Hostel
+            if (cn != null) {
+                cn.setAutoCommit(false);
+
+                ptm = cn.prepareStatement(INSERT_REPORT, Statement.RETURN_GENERATED_KEYS);
+                ptm.setString(1, report.getSendDate());
+                ptm.setString(2, report.getContent());
+                ptm.setInt(3, report.getStatus());
+                ptm.setInt(4, report.getReplyAccountID());
+                ptm.setInt(5, report.getSendAccountID());
+                ptm.setInt(6, report.getCateID());
+                check = ptm.executeUpdate() > 0;
+
+                if (!check) {
+                    cn.rollback();
+                } else {
+
+                    rs = ptm.getGeneratedKeys();
+
+                    if (rs.next()) {
+                        reportId = rs.getInt(1);
+                    }
+                    cn.commit();
+                }
+                cn.setAutoCommit(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (cn != null) {
+                cn.close();
+            }
+        }
+        return reportId;
     }
 }
