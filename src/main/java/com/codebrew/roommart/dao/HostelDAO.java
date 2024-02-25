1fc001d92daefe2994c1247b2c98284b149978f5
IamFinn7 <cpn2003.py@gmail.com>
2024-02-26 01:17:23 +0700

    Update

--- src/main/java/com/codebrew/roommart/dao/HostelDAO.java
+++ src/main/java/com/codebrew/roommart/dao/HostelDAO.java
@@ -1,6 +1,7 @@
 package com.codebrew.roommart.dao;
 
 import com.codebrew.roommart.dto.Hostel;
+import com.codebrew.roommart.dto.Room;
 import com.codebrew.roommart.utils.DatabaseConnector;
 
 import java.sql.Connection;
@@ -9,14 +10,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class HostelDAO {
-    private static final String GET_HOSTEL_BY_RENTER_ID =
-            "SELECT Hostels.hostel_id, Hostels.owner_account_id, Hostels.name, Hostels.address," +
-                    "Hostels.ward, Hostels.district, Hostels.city\n" +
-                    "FROM public.Hostels\n" +
-                    "INNER JOIN public.Rooms ON Hostels.hostel_id = Rooms.hostel_id\n" +
-                    "INNER JOIN public.Contracts ON Rooms.room_id = Contracts.room_id\n" +
-                    "INNER JOIN public.Accounts ON Contracts.renter_id = Accounts.account_id\n" +
-                    "WHERE account_id = ?";
+    private static final String
+            GET_HOSTEL_BY_RENTER_ID = "SELECT h.name, h.address, h.ward, h.district, h.city " +
+            "FROM contract_main AS cm " +
+            "INNER JOIN rooms AS r ON cm.room_id = r.room_id " +
+            "INNER JOIN hostels AS h ON r.hostel_id = h.hostel_id " +
+            "WHERE cm.renter_id = ?";
+
+
     public Hostel getHostelByRenterId(int renterId) throws SQLException {
         Connection cn = null;
         PreparedStatement pst = null;
@@ -29,55 +30,18 @@ public class HostelDAO {
                 pst.setInt(1, renterId);
                 rs = pst.executeQuery();
                 if (rs != null && rs.next()) {
-                    int hostelId = rs.getInt("hostel_id");
-                    int hostelOwnerAccountID = rs.getInt("owner_account_id");
                     String name = rs.getString("name");
                     String address = rs.getString("address");
                     String ward = rs.getString("ward");
                     String district = rs.getString("district");
                     String city = rs.getString("city");
-                    hostel = new Hostel(hostelId, hostelOwnerAccountID, name, address, ward, district, city);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pst != null) {
-                pst.close();
-            }
-            if (cn != null) {
-                cn.close();
-            }
-        }
-        return hostel;
-    }
-
-
-    //thanh - chat
-    public Hostel getHostelById(int renterId) throws SQLException {
-        Connection cn = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-        Hostel hostel = null;
-        try {
-            cn = DatabaseConnector.makeConnection();
-            if (cn != null) {
-
-                pst = cn.prepareStatement(GET_HOSTEL_BY_RENTER_ID);
-                pst.setInt(1, renterId);
-                rs = pst.executeQuery();
-                if (rs != null && rs.next()) {
-                    int hostelId = rs.getInt("hostel_id");
-                    int hostelOwnerAccountID = rs.getInt("owner_account_id");
-                    String name = rs.getString("name");
-                    String address = rs.getString("address");
-                    String ward = rs.getString("ward");
-                    String district = rs.getString("district");
-                    String city = rs.getString("city");
-                    hostel = new Hostel(hostelId, hostelOwnerAccountID, name, address, ward, district, city);
+                    hostel = Hostel.builder()
+                            .hostelName(name)
+                            .address(address)
+                            .ward(ward)
+                            .district(district)
+                            .city(city)
+                            .build();
                 }
             }
         } catch (Exception e) {
