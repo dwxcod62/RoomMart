0edabaa9a85a14748d635c91d866149ee420838f
Thanhhoacam <tienthanh1782003@gmail.com>
2024-02-29 23:09:53 +0700

    getListRooms

--- src/main/java/com/codebrew/roommart/dao/RoomDao.java
+++ src/main/java/com/codebrew/roommart/dao/RoomDao.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+//DatabaseConnector
 public class RoomDao {
     public List<Room> getListRoomsByHostelId(int hostelID) {
         Connection cn = null;
@@ -81,4 +81,75 @@ public class RoomDao {
         }
         return rooms;
     }
+    public List<Room> getListRooms() {
+        Connection cn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        ArrayList<Room> rooms = new ArrayList<>();
+        try {
+            cn = DatabaseConnector.makeConnection();
+            if (cn != null) {
+
+                // Insert new room include Nha ve sinh, cua so, cua ra vao, may lanh theo thứ tự
+                String sql = "SELECT room_id, hostel_id, room_number, capacity, room_area, has_attic, room_status, imgUrl\n" +
+                        "FROM Rooms\n";
+
+                pst = cn.prepareStatement(sql);
+
+
+                rs = pst.executeQuery();
+                if (rs != null) {
+                    while (rs.next()) {
+                        int roomID = rs.getInt("room_id");
+                        int hostelID = rs.getInt("hostel_id");
+                        int roomNumber = rs.getInt("room_number");
+                        int capacity = rs.getInt("capacity");
+                        double roomArea = rs.getDouble("room_area");
+                        int hasAttic = rs.getInt("has_attic");
+                        int roomStatus = rs.getInt("room_status");
+                        String imgUrl = rs.getString("imgUrl");
+                        RoomInformation roomInformation = null;
+                        rooms.add(Room.builder()
+                                .roomId(roomID)
+                                .hostelId(hostelID)
+                                .roomNumber(roomNumber)
+                                .roomArea(roomArea)
+                                .capacity(capacity)
+                                .roomStatus(roomStatus)
+                                .hasAttic(hasAttic)
+                                .roomInformation(roomInformation)
+                                .imgUrl(imgUrl)
+                                .build());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (cn != null) {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return rooms;
+    }
 }
+
+
