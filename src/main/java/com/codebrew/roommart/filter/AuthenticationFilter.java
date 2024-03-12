7a10a378395410e16ad5c1f1575bd3c824303fff
IamFinn7 <cpn2003.py@gmail.com>
2024-03-13 04:00:25 +0700

    update authentication

--- src/main/java/com/codebrew/roommart/filter/AuthenticationFilter.java
+++ src/main/java/com/codebrew/roommart/filter/AuthenticationFilter.java
@@ -23,45 +23,49 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException, IOException {
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        String uri = httpRequest.getRequestURI();
-//        try {
-//            int lastIndex = uri.lastIndexOf("/");
-//            String resource = uri.substring(lastIndex + 1);
-//
-//            ServletContext context = request.getServletContext();
-//            Properties authProperties = (Properties) context.getAttribute("AUTHENTICATION_lIST");
-//
-//            HttpSession session = httpRequest.getSession(true);
-//            Cookie[] c = httpRequest.getCookies();
-//            String token = null;
-//
-//            String authen = (String) authProperties.getProperty(resource);
-//            if (authen != null && authen.equals("denied") && session.getAttribute("USER") == null) {
-//                Account acc = null;
-//                if (c != null) {
-//                    for (Cookie cookie : c) {
-//                        if (cookie.getName().equals("selector")) {
-//                            token = cookie.getValue();
-//                        }
-//                        if (token != null) acc = new AccountDao().getAccountByToken(token);
-//                    }
-//                }
-//
-//                if (acc == null) {
-//                    ((HttpServletResponse) response).sendRedirect("login");
-//                } else {
-//                    session.setAttribute("CURRENT_PAGE", "dashboard");
-//                    session.setAttribute("USER", acc);
-//                    chain.doFilter(request, response);
-//                }
-//            } else {
-//                chain.doFilter(request, response);
-//            }
-//        } catch (Exception e) {
-//            System.out.println("Error in Authentication Filter : " + e);
-//        }
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String uri = httpRequest.getRequestURI();
+        try {
+            //get resource name
+            int lastIndex = uri.lastIndexOf("/");
+            String resource = uri.substring(lastIndex + 1);
 
-        chain.doFilter(request, response);
+            //get authentication properties
+            ServletContext context = request.getServletContext();
+            Properties authProperties = (Properties) context.getAttribute("AUTHENTICATION_lIST");
+            HttpSession session = httpRequest.getSession(true);
+
+            //get cookie
+            Cookie[] c = httpRequest.getCookies();
+            String token = null;
+
+            //check resource authentication
+            String rule = (String) authProperties.getProperty(resource);
+            if (rule != null && rule.equals("restricted") && session.getAttribute("USER") == null) {
+                //CHECK COOKIE
+                Account acc = null;
+                if (c != null) {
+                    for (Cookie cookie : c) {
+                        if (cookie.getName().equals("selector")) {
+                            token = cookie.getValue();
+                        }
+                    }
+                    if (token != null) acc = new AccountDao().getAccountByToken(token);
+                }
+                //NO COOKIE => LOGIN
+                if (acc == null) {
+                    ((HttpServletResponse) response).sendRedirect("loginPage");
+                } else {
+                    session.setAttribute("CURRENT_PAGE", "dashboard");
+                    session.setAttribute("USER", acc);
+                    chain.doFilter(request, response);
+                }
+
+            } else {
+                chain.doFilter(request, response);
+            }
+        } catch (Throwable t) {
+            t.getMessage();
+        }
     }
 }
