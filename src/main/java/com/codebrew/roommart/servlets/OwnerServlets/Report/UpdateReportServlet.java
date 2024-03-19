8d8685640d22bea25ccc286469d1bc174e5cc711
Thanhhoacam <tienthanh1782003@gmail.com>
2024-03-19 13:43:04 +0700

    fix report

--- src/main/java/com/codebrew/roommart/servlets/OwnerServlets/Report/UpdateReportServlet.java
+++ src/main/java/com/codebrew/roommart/servlets/OwnerServlets/Report/UpdateReportServlet.java
@@ -20,6 +20,7 @@ public class UpdateReportServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         String url = "report-detail?reportId=";
         try {
             String action = request.getParameter("action");
